175cf6529aa1a42bbe69b2cf400a6ccf75a4d350
Jeferson Andrade <j3frsn@proton.me>
2025-06-18 23:03:06 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,15 +1,17 @@
 from rest_framework import generics
 
 from blog.api.serializers import PostSerializer
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
 from blog.models import Post
 
 
 class PostList(generics.ListCreateAPIView):
-    authentication_classes = [SessionAuthentication]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly, IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
