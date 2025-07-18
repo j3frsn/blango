2b7e42dac7e93462a96a4e4c91be3f1c98d52c2a
Jeferson Andrade <j3frsn@proton.me>
2025-07-18 23:38:13 +0000

    Finish JavaScript intro and fundamentals

--- blog/views.py
+++ blog/views.py
@@ -37,6 +37,11 @@ def post_detail(request, slug):
       comment_form = None
   return render(request, "blog/post-detail.html", {"post": post, "comment_form": comment_form})
 
+
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
+
+
+def post_table(request):
+    return render(request, "blog/post-table.html")
